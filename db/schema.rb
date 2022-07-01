7615a584f7c412baac299413a1d3cd246c5b5a79
Paulina Skopp <paulina.skopp@gmail.com>
2022-07-01 11:16:30 +0200

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_111406) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_090257) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "shops", force: :cascade do |t|
@@ -29,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_111406) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
