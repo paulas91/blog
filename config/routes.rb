badb46000f434d608055ae8966a57a9ebbccaebb
Paulina Skopp <paulina.skopp@gmail.com>
2022-07-04 21:33:20 +0200

    new

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :shops
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_104706) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_04_101349) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_104706) do
     t.string "adress"
     t.string "phone"
     t.integer "workers_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthday"
+    t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
