7a2d389376a36d1bc9c19e82b7c5310b6f7393da
Paulina Skopp <paulina.skopp@gmail.com>
2022-07-10 21:34:37 +0200

    add users resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,58 @@
 class UsersController < ApplicationController 
   def index
-    @users = User.all
+    @year = params.dig(:filter, :year)&.to_i
+    # @users = if params[:filter] && params[:filter][:year] 
+    @users = if @year
+      User.where("birthday >= '#{@year}-01-01'")
+      # User.where("birthday >= '#{params[:filter][:year]}-01-01'")
+    else
+      User.all
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path, status: :see_other
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :email, :phone)
+  end
+
 end
+
+
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :birthday, :email, :phone, presence: true 
 end
