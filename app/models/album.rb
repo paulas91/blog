da889b83d8e7e843be43e601bcc31eeab35f6f21
Paulina Skopp <paulina.skopp@gmail.com>
2022-07-17 10:27:26 +0200

    Add variants for cover

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,13 @@
 class Album < ApplicationRecord
   has_many :songs
-  has_one_attached :cover #do |attachment|
+  has_one_attached :cover do |attachable|
+    attachable.variant :thumb, resize: "100x100"
+    attachable.variant :medium, resize: "300x300"
+  end
     # attachment.variant :thumb, resize_to_limit: [100, 100]
   # end
   alias_attribute :author, :band
 end
+
+
+
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,12 @@
 class Artist < ApplicationRecord
-  has_one_attached :cover 
+  has_one_attached :cover do |attachable|
+    attachable.variant :thumb, resize: "100x100" 
+    attachable.variant :medium, resize: "300x300"
+  end 
+
   has_many :songs
   validates :name, :body, :music_style, presence: true
   validates :name, uniqueness: true
+
 
 end
