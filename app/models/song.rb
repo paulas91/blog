f0654d5a038f1663c0e6593a10f35a198ff06b4b
Paulina Skopp <paulina.skopp@gmail.com>
2022-08-17 22:24:53 +0200

    create relation activity with songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ApplicationRecord
   validates :title, :body, presence: true 
   belongs_to :album
+  has_many :activities, as: :activitable
   has_one :artist, through: :album
+  
 end
