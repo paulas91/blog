59885e6c1495be7b0f7a0da856142d3aa6ab2614
Paulina Skopp <paulina.skopp@gmail.com>
2022-08-20 13:01:07 +0200

    created view songs and album with use helper

--- app/helpers/album_helper.rb
+++ app/helpers/album_helper.rb
@@ -1,2 +1,10 @@
 module AlbumHelper
+  def album_cover(album) 
+    if album.cover.attached?
+      image_tag(album.cover.variant(:thumb))
+    else
+      image_tag('No_image_available.svg', class: 'no-image')
+    end
+  end
+
 end
